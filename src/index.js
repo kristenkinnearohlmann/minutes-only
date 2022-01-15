3212162e5d16b40c66fa6cdacd476a94af2d1e1f
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2022-01-15 16:12:22 -0600

    Developing method to set timer

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 const timeInput = document.getElementById("time");
 const timeInputPlaceholder = "0m 00s";
-
-console.log("Hello world");
+const timeInputActivate = "00h 00m 00s";
+const timeAmtIndicies = [0, 1, 4, 5, 8, 9];
+let currentTimeValue;
+let currentEntryIndex;
 
 const init = () => {
   resetTimerInput();
+  currentEntryIndex = 10;
 };
 
 const startStop = () => {
@@ -18,12 +21,29 @@ const reset = () => {
 };
 
 const resetTimerInput = () => {
+  timeInput.value = "";
   timeInput.placeholder = timeInputPlaceholder;
 };
 
+const setInputCursorPosition = () => {
+  timeInput.setSelectionRange(10, 10);
+};
+
 timeInput.addEventListener("click", () => {
-  timeInput.placeholder = "00h 00m 00s";
+  timeInput.value.length == 0
+    ? (timeInput.value = timeInputActivate)
+    : (timeInput.value = timeInput.value);
+  currentTimeValue = timeInput.value;
   timeInput.style.border = "1px solid blue";
+  setInputCursorPosition();
+});
+
+timeInput.addEventListener("keyup", (e) => {
+  let currentTimeArr = currentTimeValue.split("");
+  console.log(timeAmtIndicies.reverse());
+  currentTimeArr[currentEntryIndex - 1] = e.key;
+  timeInput.value = currentTimeArr.join("");
+  setInputCursorPosition();
 });
 
 // Start app
