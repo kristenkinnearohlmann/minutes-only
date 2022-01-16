5601ac7de3d9fb5dcf4c24d0b74d9660fa4c643a
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2022-01-15 18:34:14 -0600

    Handled entry of 0, started work on start stop

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const timeInputPlaceholder = "0m 00s";
 const timeInputActivate = "00h 00m 00s";
 const timeAmtIndicies = [0, 1, 4, 5, 8, 9];
 let currentTimeValue;
+let currentTimeArr;
 
 const init = () => {
   resetTimerInput();
@@ -11,6 +12,11 @@ const init = () => {
 const startStop = () => {
   console.log("Clicked Start Stop");
   console.log(currentTimeValue);
+  const currentTimeVals = getCurrentTimeValues();
+  console.log(currentTimeVals);
+  console.log(currentTimeVals[0], currentTimeVals[1]);
+  console.log(currentTimeVals[2], currentTimeVals[3]);
+  console.log(currentTimeVals[4], currentTimeVals[5]);
 };
 
 const reset = () => {
@@ -24,6 +30,16 @@ const resetTimerInput = () => {
 
 const setInputCursorPosition = () => {
   timeInput.setSelectionRange(10, 10);
+};
+
+const getCurrentTimeValues = () => {
+  currentTimeArr = currentTimeValue.split("");
+
+  return currentTimeArr.filter((val, idx) => {
+    if (timeAmtIndicies.includes(idx)) {
+      return val;
+    }
+  });
 };
 
 document.addEventListener("click", (e) => {
@@ -45,17 +61,12 @@ timeInput.addEventListener("click", () => {
 });
 
 timeInput.addEventListener("keyup", (e) => {
-  if (!parseInt(e.key)) {
+  console.log(e.key);
+  if (!parseInt(e.key) && e.key !== "0") {
     timeInput.value = currentTimeValue;
     setInputCursorPosition();
   } else {
-    let currentTimeArr = currentTimeValue.split("");
-
-    const currentTimeVals = currentTimeArr.filter((val, idx) => {
-      if (timeAmtIndicies.includes(idx)) {
-        return val;
-      }
-    });
+    const currentTimeVals = getCurrentTimeValues();
 
     currentTimeVals.shift();
     currentTimeVals.push(e.key);
