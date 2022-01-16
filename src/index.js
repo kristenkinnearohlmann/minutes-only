f5c3fa9ad6fc19f02c2183d047acf7cbd8710a61
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2022-01-15 19:43:49 -0600

    Abstracted function to process time to minutes and seconds, updated calling functions

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ const btnStartStop = document.getElementById("start-stop");
 const btnReset = document.getElementById("reset");
 const timeInputPlaceholder = timeInput.placeholder;
 const timeInputActivate = "00h 00m 00s";
-const timeAmtIndicies = [0, 1, 4, 5, 8, 9];
+const timeInputAmtIndicies = [0, 1, 4, 5, 8, 9];
+const timeDisplayAmtIndicies = [0, 1, 4, 5];
 let currentTimeValue;
 let currentTimeArr;
 let isRunning = false;
+let timerInterval;
 
 const init = () => {
   resetTimerInput();
@@ -19,13 +21,18 @@ const startStop = (e) => {
     btnStartStop.innerText = "Stop";
   } else {
     btnStartStop.innerText = "Start";
+    stopTimer();
     return;
   }
 
-  const currentTimeVals = getCurrentTimeValues();
-  let inputHours = parseInt(currentTimeVals[0] + currentTimeVals[1]);
-  let inputMinutes = parseInt(currentTimeVals[2] + currentTimeVals[3]);
-  let inputSeconds = parseInt(currentTimeVals[4] + currentTimeVals[5]);
+  const currentTimeVals = getInputTimeValues(
+    currentTimeValue,
+    timeInputAmtIndicies
+  );
+
+  let inputHours = parseTimeIncrement(currentTimeVals[0], currentTimeVals[1]);
+  let inputMinutes = parseTimeIncrement(currentTimeVals[2], currentTimeVals[3]);
+  let inputSeconds = parseTimeIncrement(currentTimeVals[4], currentTimeVals[5]);
 
   if (inputHours > 0) {
     inputMinutes += inputHours * 60;
@@ -41,12 +48,14 @@ const startStop = (e) => {
     .padStart(2, 0)}s`;
   timeInput.value = "";
   isRunning = true;
+  timerInterval = setInterval(updateTimer, 1000);
 };
 
 const reset = () => {
   isRunning = false;
   currentTimeValue = "";
   btnStartStop.innerText = "Start";
+  stopTimer();
   resetTimerInput();
 };
 
@@ -59,14 +68,45 @@ const setInputCursorPosition = () => {
   timeInput.setSelectionRange(10, 10);
 };
 
-const getCurrentTimeValues = () => {
-  currentTimeArr = currentTimeValue.split("");
+const getInputTimeValues = (timeValue, timeIndicies) => {
+  currentTimeArr = timeValue.split("");
 
   return currentTimeArr.filter((val, idx) => {
-    if (timeAmtIndicies.includes(idx)) {
+    if (timeIndicies.includes(idx)) {
       return val;
     }
   });
+};
+
+const updateTimer = () => {
+  console.log("Running...");
+  const updateTimerVal = getInputTimeValues(
+    timeInput.placeholder,
+    timeDisplayAmtIndicies
+  );
+  console.log(updateTimerVal);
+  let inputMinutes = parseTimeIncrement(updateTimerVal[0], updateTimerVal[1]);
+  let inputSeconds = parseTimeIncrement(updateTimerVal[2], updateTimerVal[3]);
+
+  console.log(inputMinutes);
+  console.log(inputSeconds);
+  console.log(inputMinutes * 60 + inputSeconds);
+};
+
+const stopTimer = () => {
+  console.log("Stopping timer");
+  clearInterval(timerInterval);
+};
+
+const parseTimeIncrement = (pos1, pos2) => {
+  console.log(pos1, pos2);
+  if (pos1 === "0" && pos2 === "0") return 0;
+
+  if (pos1 === "0") {
+    return parseInt(pos2);
+  } else {
+    return parseInt(pos1 + pos2);
+  }
 };
 
 document.addEventListener("click", (e) => {
@@ -92,13 +132,16 @@ timeInput.addEventListener("keyup", (e) => {
     timeInput.value = currentTimeValue;
     setInputCursorPosition();
   } else {
-    const currentTimeVals = getCurrentTimeValues();
+    const currentTimeVals = getInputTimeValues(
+      currentTimeValue,
+      timeInputAmtIndicies
+    );
 
     currentTimeVals.shift();
     currentTimeVals.push(e.key);
 
-    for (let i = timeAmtIndicies.length - 1; i >= 0; i--) {
-      currentTimeArr[timeAmtIndicies[i]] = currentTimeVals.pop();
+    for (let i = timeInputAmtIndicies.length - 1; i >= 0; i--) {
+      currentTimeArr[timeInputAmtIndicies[i]] = currentTimeVals.pop();
     }
 
     currentTimeValue = currentTimeArr.join("");
