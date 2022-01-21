867be70cdd729e3d3cba6be2cbfa45a4623766b7
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2022-01-20 20:54:39 -0600

    Started draft of convertTimeValue

--- src/index.js
+++ src/index.js
@@ -2,25 +2,56 @@ const timeEntry = document.getElementById("time-entry");
 const timeEntryPlaceholder = timeEntry.placeholder;
 const timeEntryActivatePlaceholder = "00h 00m 00|s";
 const timeInputAmtIndicies = [0, 1, 4, 5, 8, 9];
+const timeMinSecIndicies = [0, 1, 4, 5];
 const btnStartStop = document.getElementById("start-stop");
 const btnReset = document.getElementById("reset");
 let isRunning;
-let timeRemaining;
+let timeRemaining = [];
 let inputTimeArray = [];
 let timeIncrements = [];
 let entryTime = [];
+let inputHours;
+let inputMinutes;
+let inputSeconds;
 
 const init = () => {
   isRunning = false;
 };
 
 // functions
-const getInputTimeArray = (timeValue, timeIndicies) => {
-  return timeValue.split("").filter((val, idx) => {
+const convertTimeValue = (timeValues) => {
+  console.log(timeValues);
+  console.log(timeValues.length);
+
+  if (timeValues.length === 6) {
+    inputHours = parseTimeIncrement(timeValues[0], timeValues[1]);
+    inputMinutes = parseTimeIncrement(timeValues[2], timeValues[3]);
+    inputSeconds = parseTimeIncrement(timeValues[4], timeValues[5]);
+  } else if (timeValues.length === 4) {
+    inputMinutes = parseTimeIncrement(timeValues[0], timeValues[1]);
+    inputSeconds = parseTimeIncrement(timeValues[2], timeValues[3]);
+  } else {
+    inputMinutes = 0;
+    inputSeconds = parseTimeIncrement(timeValues[0], timeValues[1]);
+  }
+};
+
+const getInputTimeArray = (entryTimeValue, timeIndicies) => {
+  return entryTimeValue.split("").filter((val, idx) => {
     if (timeIndicies.includes(idx)) {
       return val;
     }
   });
+};
+
+const parseTimeIncrement = (pos1, pos2) => {
+  if (pos1 === "0" && pos2 === "0") return 0;
+
+  if (pos1 === "0") {
+    return parseInt(pos2);
+  } else {
+    return parseInt(pos1 + pos2);
+  }
 };
 
 const reset = () => {
@@ -52,7 +83,9 @@ const setTimerValue = (timeValueEntered) => {
     timeEntry.placeholder,
     timeInputAmtIndicies
   );
-  console.log(timeIncrements);
+
+  timeRemaining = convertEntryTime(timeIncrements);
+  console.log(timeRemaining);
 };
 
 const timerStart = () => {
